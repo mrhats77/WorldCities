77aa1171dfd6ab8a9d2c697ca5466b93c3a3331f
Wil <wilgermunguia@pm.me>
2023-05-30 08:58:03 -0600

    create new city with a country selector form

--- src/app/cities/city-edit.component.ts
+++ src/app/cities/city-edit.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 import { environment } from './../../environments/environment';
 import { City } from './city';
+import { Country } from './../countries/country';
 @Component({
   selector: 'app-city-edit',
   templateUrl: './city-edit.component.html',
@@ -20,6 +21,8 @@ export class CityEditComponent implements OnInit {
   // It's NULL when we're adding a new city,
   // and not NULL when we're editing an existing one.
   id?: number;
+  // the countries array for the select
+  countries?: Country[];
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -29,11 +32,14 @@ export class CityEditComponent implements OnInit {
     this.form = new FormGroup({
       name: new FormControl(''),
       lat: new FormControl(''),
-      lon: new FormControl('')
+      lon: new FormControl(''),
+      countryId: new FormControl('')
     });
     this.loadData();
   }
   loadData() {
+    // load countries
+    this.loadCountries();
     // retrieve the ID from the 'id' parameter
     var idParam = this.activatedRoute.snapshot.paramMap.get('id');
     this.id = idParam ? +idParam : 0;
@@ -53,12 +59,24 @@ export class CityEditComponent implements OnInit {
       this.title = "Create a new City";
     }
   }
+  loadCountries() {
+    // fetch all the countries from the server
+    var url = environment.baseUrl + 'api/Countries';
+    var params = new HttpParams()
+      .set("pageIndex", "0")
+      .set("pageSize", "9999")
+      .set("sortColumn", "name");
+    this.http.get<any>(url, { params }).subscribe(result => {
+      this.countries = result.data;
+    }, error => console.error(error));
+  }
   onSubmit() {
     var city = (this.id) ? this.city : <City>{};
     if (city) {
       city.name = this.form.controls['name'].value;
       city.lat = +this.form.controls['lat'].value;
       city.lon = +this.form.controls['lon'].value;
+      city.countryId = +this.form.controls['countryId'].value;
       if (this.id) {
         // EDIT mode
         var url = environment.baseUrl + 'api/Cities/' + city.id;
